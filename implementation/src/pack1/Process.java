376a5434bb408e45fbc300baa12960c1f23ef652
fadiakhaled <110059717+fadiakhaled@users.noreply.github.com>
2022-12-07 17:06:59 +0200

    Created process class

---
+++ implementation/src/pack1/Process.java
@@ -0,0 +1,43 @@
+package pack1;
+
+public class Process {
+    private String name;
+    private int burstTime;
+    private int arrivalTime;
+    private int priority;
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+
+}
+
