023508fadfd45db1fc974d35b114b17c960fc7e6
fadiakhaled <110059717+fadiakhaled@users.noreply.github.com>
2022-12-25 19:12:43 +0200

    Update AGScheduler.java

--- implementation/src/pack1/AGScheduler.java
+++ implementation/src/pack1/AGScheduler.java
@@ -46,9 +46,9 @@ public class AGScheduler {
         newProcess = currentProcess;
 
         int currentIndex = processes.indexOf(currentProcess);
-        for (int i =0; i < processes.size(); i++) {
+        for (int i = 0; i < processes.size(); i++) {
             if (processes.get(i).getBurstTime() > 0) {
-                if (burstCopy[i] < burstCopy[currentIndex]  && processes.get(i).getArrivalTime() <= time ) {
+                if (burstCopy[i] < burstCopy[currentIndex] && processes.get(i).getArrivalTime() <= time) {
                     newProcess = processes.get(i);
                     flag = true;
                 }
@@ -67,7 +67,7 @@ public class AGScheduler {
 
         int[] quantum = new int[size];
         int[] burstCopy = new int[size];
-        int [] burstCopy2 = new int [size];
+        int[] burstCopy2 = new int[size];
         for (int i = 0; i < size; i++) {
             quantum[i] = processes.get(i).getQuantum();
             burstCopy[i] = processes.get(i).getBurstTime();
@@ -95,13 +95,13 @@ public class AGScheduler {
                             currentProcess = processes.get(flag);
                             currentIndex = flag;
                             amount = min((int) ceil(0.25 * currentProcess.getQuantum()), quantum[currentIndex]);
-                            amount = min (amount, burstCopy[currentIndex]);
+                            amount = min(amount, burstCopy[currentIndex]);
 
                             quantum[currentIndex] -= amount;
-                                    time += amount;
+                            time += amount;
                             burstCopy[currentIndex] -= amount;
                             switched = false;
-                        }else {
+                        } else {
                             quantum[flag] = currentProcess.getQuantum() + 2;
                             currentProcess.setQuantum(quantum[flag]);
                             switched = true;
@@ -119,13 +119,11 @@ public class AGScheduler {
             }
 
 
-
             String currentProcessName = currentProcess.getName();
             int orderSize = executionOrder.size();
             if (executionOrder.size() == 0) {
                 executionOrder.add(currentProcess.getName());
-            }
-            else if (!Objects.equals(currentProcessName, executionOrder.get(orderSize-1))) {
+            } else if (!Objects.equals(currentProcessName, executionOrder.get(orderSize - 1))) {
                 executionOrder.add(currentProcessName);
             }
 
@@ -153,16 +151,16 @@ public class AGScheduler {
                         time -= amount;
                         quantum[currentIndex] += amount;
                         burstCopy[currentIndex] += amount;
-                            amount = min((int) ceil(0.5 * currentProcess.getQuantum()), quantum[currentIndex]);
-                            amount = min (amount, burstCopy[currentIndex]);
+                        amount = min((int) ceil(0.5 * currentProcess.getQuantum()), quantum[currentIndex]);
+                        amount = min(amount, burstCopy[currentIndex]);
                         quantum[currentIndex] -= amount;
-                            time += amount;
+                        time += amount;
                         burstCopy[currentIndex] -= amount;
 
                         //if (burstCopy[currentIndex] < 0) burstCopy[currentIndex] = 0;
                     } else {
                         quantum[currentIndex] = currentProcess.getQuantum() + 2;
-                            switched = true;
+                        switched = true;
                         flag = 0;
                         continue;
                     }
@@ -179,7 +177,7 @@ public class AGScheduler {
                 // p1 : 0 2 4 , p2 : 4 7 , p3 : 7 9, p2: 9 12,
             } else {
                 int remaining = currentProcess.getQuantum() - amount;
-                quantum[currentIndex] = (int) (currentProcess.getQuantum() + ceil(remaining/2.0));
+                quantum[currentIndex] = (int) (currentProcess.getQuantum() + ceil(remaining / 2.0));
                 currentProcess.setQuantum(quantum[currentIndex]);
                 currentProcess = newProcess;
                 currentIndex = processes.indexOf(newProcess);
@@ -190,7 +188,7 @@ public class AGScheduler {
             }
 
 
-            if (quantum[currentIndex] <= 0 && burstCopy[currentIndex] > 0 ) {
+            if (quantum[currentIndex] <= 0 && burstCopy[currentIndex] > 0) {
                 quantum[currentIndex] = currentProcess.getQuantum() + 2;
                 currentProcess.setQuantum(quantum[currentIndex]);
                 switched = true;
@@ -206,20 +204,18 @@ public class AGScheduler {
                         time -= amount;
                         quantum[currentIndex] += amount;
                         burstCopy[currentIndex] += amount;
-                            if (count == 0) {
-                            amount = (int)min( ceil(0.75 * currentProcess.getQuantum()), quantum[currentIndex]);
-                            amount = min (amount, burstCopy[currentIndex]);
-
-                                quantum[currentIndex] -= amount;
-                                    time += amount;
+                        if (count == 0) {
+                            amount = (int) min(ceil(0.75 * currentProcess.getQuantum()), quantum[currentIndex]);
+                            amount = min(amount, burstCopy[currentIndex]);
+                            quantum[currentIndex] -= amount;
+                            time += amount;
                             burstCopy[currentIndex] -= amount;
                             count++;
                         } else {
                             amount = min(currentProcess.getQuantum(), quantum[currentIndex]);
-                            amount = min (amount, burstCopy[currentIndex]);
-
-                                quantum[currentIndex] -= amount;
-                                    time += amount;
+                            amount = min(amount, burstCopy[currentIndex]);
+                            quantum[currentIndex] -= amount;
+                            time += amount;
                             burstCopy[currentIndex] -= amount;
                             count++;
                         }
@@ -229,8 +225,7 @@ public class AGScheduler {
                         switched = true;
                         break;
                     }
-                }
-                else {
+                } else {
                     int waiting = (int) (time - currentProcess.getArrivalTime() - currentProcess.getBurstTime());
                     currentProcess.setWaitingTime(waiting);
                     quantum[currentIndex] = 0;
